93188a935d4630d499b1f557972299f6691dcd37
iliaskap12 <iliaskapareliotis@gmail.com>
2021-01-26 13:28:33 +0200

    Connected Database locally

--- server/database/index.js
+++ server/database/index.js
@@ -3,7 +3,6 @@ let { Pool } = require('pg');
 const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/weather-db';
 const SSL = process.env.NODE_ENV === 'production';
 
-
 class Database {
     constructor () {
         this._pool = new Pool({
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// var db = require('./database');
+let db = require('./database');
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
@@ -14,6 +14,13 @@ app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}!`);
+});
+
+db.query('SELECT NOW()', (err, res) => {
+    if(err.error) {
+        return console.log(err.error);
+    }
+    console.log(`PostgreSQL connected: ${res[0].now}.`);
 });
 
 module.exports = app;
